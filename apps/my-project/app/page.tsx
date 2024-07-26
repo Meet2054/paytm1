6f43bac7d8696d9b331188e0d34d0cd610de538b
Meet2054 <meetaparekh2003@gmail.com>
2024-07-27 01:28:44 +0530

    Inital setup of prisma

--- apps/my-project/app/page.tsx
+++ apps/my-project/app/page.tsx
@@ -1,13 +1,14 @@
-// eslint-disable-next-line react/no-children-prop
-import Image from "next/image";
+import { PrismaClient } from "@repo/db/client";
 import {Button} from "../../../packages/ui/src/button";
+
+const client = new  PrismaClient();
+
+
 export default function Home() {
   return (
     <div className="text-3xl" >
       <h1>Hi here </h1>
-      <Button appName={""}>
-        {undefined}
-      </Button>
+      <Button  />
     </div>
   );
 }
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,14 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
-
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
-}
-
-export const Button = ({ children, appName }: ButtonProps) => {
+export const Button = () => {
   return (
     <button
       className="w-auto h-auto border-2 p-1 border-black rounded-md" 
